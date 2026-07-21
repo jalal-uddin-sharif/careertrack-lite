6a28ac3aca98dd422a4051323ef04aa884e1b39d
jalal-uddin-sharif <itsharifofficial@gmail.com>
2026-07-21 12:45:37 +0600

    setup mongodb connection

---
+++ server/src/db/connect.js
@@ -0,0 +1,12 @@
+const { MongoClient } = require("mongodb");
+let db;
+const connectDB = async () => {
+  if (db) return db;
+  const client = new MongoClient(process.env.MONGO_URI);
+  await client.connect();
+  db = client.db("CareerTrack Lite");
+  console.log("Mongodb connected");
+  return db;
+};
+
+module.exports = connectDB
--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,23 @@
 const express = require('express');
-const app = express();
+require("dotenv").config()
+const dns = require("dns")
+dns.setServers(["1.1.1.1", "8.8.8.8"])
 const port = 3000;
 
+const app = express();
+app.use(express.json())
+
+const connectDB = require("./db/connect")
+
+//mongodb connection test
+connectDB().then(()=>{
+  console.log("Mongodb connection success");
+}).catch(err =>{
+  console.log("mongodb connection fail", err);
+})
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('api is running');
 });
 
 app.listen(port, () => {
